57cd5505d774d1cfe3ed6e15eb3b15a0d681925c
Quest <279519897@qq.com>
2023-12-23 17:45:26 +0800

    第二次提交

---
+++ src/main/java/com/example/somecode/SomeCodeApplication.java
@@ -0,0 +1,13 @@
+package com.example.somecode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SomeCodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SomeCodeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/somecode/SomeCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.somecode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SomeCodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
